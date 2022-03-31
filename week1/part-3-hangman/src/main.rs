6229637f99d2da6d87b3eb11c9934545a4de0a78
zxr001119 <zxr001119@mail.ustc.edu.cn>
2022-03-31 19:19:43 +0800

    finished week2 execrise

--- week1/part-3-hangman/src/main.rs
+++ week1/part-3-hangman/src/main.rs
@@ -32,9 +32,51 @@ fn main() {
     // Note: given what you know about Rust so far, it's easier to pull characters out of a
     // vector than it is to pull them out of a string. You can get the ith character of
     // secret_word by doing secret_word_chars[i].
-    let secret_word_chars: Vec<char> = secret_word.chars().collect();
+    let mut secret_word_chars: Vec<char> = secret_word.chars().collect();
     // Uncomment for debugging:
     // println!("random word: {}", secret_word);
 
     // Your code here! :)
+    let mut counter = NUM_INCORRECT_GUESSES;
+    let mut existingString = String::from("");
+    let mut guessChars = String::from("");
+    let length = secret_word.len();
+    for i in 0..length {
+        existingString.push_str("-");
+    }
+
+    println!("Welcome to CS110L Hangman!");
+    while (counter > 0 && existingString != secret_word) {
+        println!("The word so far is {}", existingString);
+        println!("You have guessed the following letters: {}", guessChars);
+        println!("You have {} guesses left", counter);
+        print!("Please guess a letter: ");
+
+        io::stdout().flush().expect("Error flushing stdout.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Error reading line.");
+
+        let newChar = guess.chars().next().unwrap();
+        guessChars.push_str(&(newChar.to_string()));
+        if (secret_word_chars.contains(&newChar)){
+            for index in 0..length {
+                if (secret_word_chars[index] == newChar) {
+                    secret_word_chars[index] = '-';
+                    existingString.replace_range(index..index+1, &(newChar.to_string()));
+                    break;
+                }
+            }
+        } else {
+            counter = counter - 1;
+        }
+        println!();
+    }
+
+    if (counter > 0) {
+        println!("Congratulations you guessed the secret word: {}!", secret_word);
+    } else {
+        println!("Sorry, you ran out of guesses!");
+    }
 }
